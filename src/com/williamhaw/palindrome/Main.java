e675bd6b8c11e20cf29eb54ffac93de5f776d447
william <z59sgs@gmail.com>
2018-06-01 22:35:07 +0800

    Setup project

---
+++ src/com/williamhaw/palindrome/Main.java
@@ -0,0 +1,15 @@
+package com.williamhaw.palindrome;
+
+/**
+ * Entry point for application
+ * @author williamhaw
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
